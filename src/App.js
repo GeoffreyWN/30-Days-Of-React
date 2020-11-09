e20f53cf8b605ef8249ca60cb5b34ab5f55b32d9
geof <ndungugeofrrey11@gmail.com>
2020-11-09 12:13:30 +0300

    changed background color based on season

--- src/App.js
+++ src/App.js
@@ -13,13 +13,41 @@ class App extends Component {
    hexatitle: 'Hexadecimal Colours',
    color_array: Array.from(Array(32), (_, i) =>i + 1),
    testArray: Array(50).fill().map((_, i)=>(i +1)),
-   populationTitle: 'World Population'
+   populationTitle: 'World Population',
  }
 
- 
+
   render() {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const Spring = [ "March", "April", "May"]
+    const Summer = ["June", "July", "August",]
+    const Autumn = ["September", "October", "November"]
+    const Winter = ["December", "January", "February"]
+    const month = new Date().getMonth()
+    // const month = "January"
+
+    const findCurrentSeason =() => {
+      let season;
+      if (Spring.includes(months[month])) {
+        season = 'Spring' 
+     }
+      else if (Summer.includes(months[month])) {
+        season = 'Summer'
+     }
+      else if (Autumn.includes(months[month])) {
+        season = 'Autumn'
+     }
+      else if (Winter.includes(months[month])) {
+        season = 'Winter'
+     }
+     console.log(season)
+     return season
+    }
+    
+ 
+    // const Autumn = new
     return (
-      <div className="App">
+      <div className={`App ${findCurrentSeason()}`}>
         <Header title={this.state.title} />
         <Number numbers={this.state.numbers} numberstitle={this.state.numberstitle} />
         <Hexadecimal  hexatitle={this.state.hexatitle} color_array={this.state.color_array}/>
